436af6357e7018909baf81bb189e2b01d4eff45e
IvanR <ivan.ramirezf@gmail.com>
2024-10-21 23:35:06 +1000

    Short commission

---
+++ 006_short_circuiting.js
@@ -0,0 +1,10 @@
+// Short circuiting and logical operators
+
+console.log(true && "Some string");
+console.log(false && "Some string");
+
+hasMovieAdaptation = true;
+console.log(hasMovieAdaptation && "This book has a movie");
+
+console.log(true || "Some string");
+console.log(false || "Some string");
