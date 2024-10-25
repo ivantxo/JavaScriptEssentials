be91806e0e97784a96b4f666b1546ee324d18d32
IvanR <ivan.ramirezf@gmail.com>
2024-10-26 00:32:12 +1000

    Promises / Async await fix

--- 013_promises_async_await.js
+++ 013_promises_async_await.js
@@ -6,16 +6,16 @@
 // console.log("ivan");
 
 // Async/Await
-  // async function getTodos() {
-  // async function getTodos() {
-  //   await fetch("https://jsonplaceholder.typicode.com/todos")
-  //     .then((res) => res.json())
-  //     .then((data) => console.log(data));
+// async function getTodos() {
+//   await fetch("https://jsonplaceholder.typicode.com/todos")
+//     .then((res) => res.json())
+//     .then((data) => console.log(data));
 
-  //   return data;
-  // }
-  // const todos = getTodos();
+//   return data;
+// }
+// const todos = getTodos();
 
+async function getTodos() {
   const res = await fetch("https://jsonplaceholder.typicode.com/todos");
   const data = await res.json();
   console.log("data", data);
